3dfb51fb85323795ed8be42db7b4de8ee3d321f6
K-shir0 <kakishiro2190394@gmail.com>
2020-12-22 18:21:05 +0900

    feat: appをapp.dartに切り分け

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:tp_calculate/main.dart';
+import 'package:tp_calculate/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
