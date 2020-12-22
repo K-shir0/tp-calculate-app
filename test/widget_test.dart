011ae2917b13743de2c20278843b9bbcd742f5a4
K-shir0 <kakishiro2190394@gmail.com>
2020-12-22 18:34:56 +0900

    feat: Providerを追加

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:tp_calculate/app.dart';
+import 'package:tp_calculate/screens/example/example.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
