6ad73f2fcb468557ee424992a1a62e862587778e
K-shir0 <kakishiro2190394@gmail.com>
2020-12-22 20:15:16 +0900

    feat: CalculateTextFieldのデザインを調整

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -7,41 +7,59 @@ class HomeScreen extends HookWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          title: Text("TP Calculate")
-      ),
+      appBar: AppBar(title: Text("TP Calculate")),
       body: Container(
         padding: EdgeInsets.all(16),
-        child: Column(
-          children: [
-            CalculateForm()
-          ],
-        ),
+        child: CalculateForm(),
       ),
     );
   }
 }
 
+// Molecules
+// ・有効性の確認
+// ・リクエスト等
 class CalculateForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: TPTextField(),
+      child: Column(
+        children: [
+          CalculateTextField(label: "PERFECT"),
+          Row(
+            children: [
+              Expanded(child: CalculateTextField(label: "PERFECT")),
+              Expanded(child: CalculateTextField(label: "PERFECT")),
+              Expanded(child: CalculateTextField(label: "PERFECT")),
+              Expanded(child: CalculateTextField(label: "PERFECT")),
+            ],
+          )
+        ],
+      ),
     );
   }
-
 }
 
-class TPTextField extends StatelessWidget {
+// Atoms
+class CalculateTextField extends StatelessWidget {
+  final String label;
+  final bool hasError;
+
+  const CalculateTextField({Key key, this.label, this.hasError}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return TextField(
-      decoration: new InputDecoration(labelText: "Enter your number"),
+      decoration: new InputDecoration(
+        labelText: label,
+        enabledBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.red)
+        )
+      ),
       keyboardType: TextInputType.number,
       inputFormatters: <TextInputFormatter>[
         FilteringTextInputFormatter.digitsOnly
       ],
     );
   }
-
 }
