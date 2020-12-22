a5c7380a281f84c944d2938114f13eaddeee7eee
K-shir0 <kakishiro2190394@gmail.com>
2020-12-22 18:47:43 +0900

    feat: ホームを追加

--- lib/app.dart
+++ lib/app.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:tp_calculate/screens/example/example.dart';
+import 'package:tp_calculate/screens/home/home.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       onGenerateRoute: (settings) {
+        // Home
+        if (settings.name == '/') {
+          return MaterialPageRoute(builder: (context) => HomeScreen());
+        }
+
         return MaterialPageRoute(builder: (context) => MyApp());
       },
     );
---
+++ lib/screens/home/home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+class HomeScreen extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("TP Calculate")
+      ),
+      body: Container(
+
+      ),
+    );
+  }
+  
+}
