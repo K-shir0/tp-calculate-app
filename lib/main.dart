1f086d7059a4dea14dd8fad235652e638db2491a
K-shir0 <kakishiro2190394@gmail.com>
2020-12-27 02:30:57 +0900

    feat: .envを読み込むようにした

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'app.dart';
 
-void main() {
+void main() async {
+  // 環境変数ファイルの読み込み
+  await DotEnv().load('.env');
+
   runApp(
     ProviderScope(
       child: App(),
